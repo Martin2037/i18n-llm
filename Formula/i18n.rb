1e140f2b4f0676afb0744859b4d2759d80134f87
Administrator <zhujunyang@futurevista.work>
2024-08-26 16:19:16 +0800

    :hammer: update

--- Formula/i18n.rb
+++ Formula/i18n.rb
@@ -1,9 +1,10 @@
 class I18n < Formula
   desc "AI-Powered Internationalization Translation Tool"
   homepage "https://github.com/Martin2037/i18n-llm"
-  url "https://github.com/Martin2037/i18n-llm/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "5fa9c08cccd78ae51bc4d71a75b947844ca2653bbd262801c628c3d52c01fba3"
+  url "https://github.com/Martin2037/i18n-llm/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "d18b274d07781dadc8d6a76867a09017d7343df980e191068d69b9591f75811d"
   license "MIT"
+  version "0.0.2"
 
   depends_on "go" => :build
 
@@ -12,6 +13,6 @@ class I18n < Formula
   end
 
   test do
-    assert_match "i18n version 0.0.1", shell_output("#{bin}/i18n --version")
+    assert_match "i18n version 0.0.2", shell_output("#{bin}/i18n --version")
   end
 end
